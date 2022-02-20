1b60571a4cb2d1b4cb7c4b4816dff4998495aaee
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-02-20 16:39:13 -0500

    added psuesdocde

---
+++ 0-lowercase-domains.js
@@ -0,0 +1,10 @@
+/* creating a function that turns any string into loweercase
+PARAMETER- STRING
+RETURN- STRING IN LOWER LETTERS
+FUNCTION NAME- lowerCase
+set newString[]
+for each letter in string{
+    newString= lowercase string letter
+}
+ return newString
+*/
