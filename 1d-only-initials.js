03cd1f28e4a9b2d11483d2ad8d3dbdbab3e37b3e
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-03-09 18:52:36 -0500

    Added only initials

--- 1d-only-initials.js
+++ 1d-only-initials.js
@@ -1,5 +1,15 @@
 function onlyInitals(names){
-
+    let initals =[];
+    for (let name of names){
+        let intial = "";
+        for (let i=0; i< name.length;i++){
+            if (name.charAt(i) === " "){
+                intial = name.charAt(0)+"."+name.charAt(i+1)+".";
+            }            
+        }
+        initals.push(intial);
+    }
+    return initals;
 }
 
 console.log(onlyInitals(["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"]) ,"A.K.", "L.H.", "Y.M.");
---
+++ 1f-pokemon-type.js
@@ -0,0 +1,6 @@
+function pokemonTypes(pokemon){
+    let pokemonString=[];
+    return pokemonString;
+}
+
+console.log(pokemonTypes({ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]), "[Bulbasaur is a grass type Pokémon., Charmander is a fire type Pokémon., Squirtle is a water type Pokémon.]" );
