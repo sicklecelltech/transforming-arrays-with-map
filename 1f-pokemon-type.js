877fd3e3a272acf21666f1e8ed6932d5b4fe0c4d
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-03-09 19:18:32 -0500

    added pokemon types

--- 1f-pokemon-type.js
+++ 1f-pokemon-type.js
@@ -1,6 +1,10 @@
-function pokemonTypes(pokemon){
+function pokemonTypes(pokemons){
     let pokemonString=[];
+    for (let pokemon of pokemons){
+        let str = pokemon.name +" is a "+pokemon.type+" pokemon.";
+        pokemonString.push(str);
+    }
     return pokemonString;
 }
 
-console.log(pokemonTypes({ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]), "[Bulbasaur is a grass type Pokémon., Charmander is a fire type Pokémon., Squirtle is a water type Pokémon.]" );
+console.log(pokemonTypes([{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]), "[Bulbasaur is a grass type Pokémon., Charmander is a fire type Pokémon., Squirtle is a water type Pokémon.]" );
